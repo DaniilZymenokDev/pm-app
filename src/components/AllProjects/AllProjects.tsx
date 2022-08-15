c845e2fc9819aadfb24e23740491fde5364a38a1
Zimenokk <dzimenokk@gmail.com>
2022-08-15 12:59:53 +0300

    stepper work

--- src/components/AllProjects/AllProjects.tsx
+++ src/components/AllProjects/AllProjects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from "../../layout/Layout";
 import styles from './AllProjects.module.scss'
 import ActiveLastBreadcrumb from "../basic-components/Breadcrumb/ActiveLastBreadcrumb";
@@ -6,16 +6,23 @@ import Search from "../../design-core/Search";
 import {Button} from "@mui/material";
 import SvgSelector from "../basic-components/SvgSelector/SvgSelector";
 import ConnectionsTable from "./ConnectionsTable/ConnectionsTable";
+import HorizontalNonLinearStepper from "../NewConnectionStepper/NewConnection";
 
 const AllProjects: React.FC = () => {
+    const [isModal, setIsModal] = useState(true);
+
+    const openModalWindow = (): void => {
+        setIsModal(!isModal)
+    }
+
     return (
         <Layout>
+            {isModal && <HorizontalNonLinearStepper active={isModal} setActive={setIsModal}/>}
             <div className={styles.tableHeader}>
                 <ActiveLastBreadcrumb/>
                 <div className={styles.tableHeaderActions}>
-
                     <Search/>
-                    <Button variant="contained">
+                    <Button variant="contained" onClick={openModalWindow}>
                         <SvgSelector id={"Add"}/>
                         Add new Connection </Button>
                 </div>
